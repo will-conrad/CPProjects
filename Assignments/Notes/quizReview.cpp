69f67dbf9c326fe89467d766b6f99f724e412b5d
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-04-12 08:22:45 -0400

    quiz

--- Assignments/Notes/quizReview.cpp
+++ Assignments/Notes/quizReview.cpp
@@ -1,6 +1,31 @@
 /*
+---QUIZ REVIEW---
+All on functions
+10 Qs – 25pts
 
+Combo, Fill in the blank, multiple choice, writing code
+3 parts of function, return types.
 
+Where do functions, prototypes go: before main()
+Where do definitions go: after main()
+Where do calls go: In main() or in functions
+	- call function by name with required parameters
+	- void student(string name){} = student("matt");
+how to make a function
+pass by value vs pass by & reference
+
+know what recursion is, relate an example.
+Recursive functions
+- a way of breaking a larger problem into its base steps, then building back up to the larger problem
+dont know 2^7
+know 2*2
+build back up
+
+7^5
+  ↳7*7^4
+	   ↳7*7^3
+	   		↳7*7^2
+	   			 ↳7*7
 
 
 
