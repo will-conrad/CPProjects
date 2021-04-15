61bca4f90bd3c02f912bfaeeb3b2f70c2121f859
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-04-15 14:08:35 -0400

    Array notes

---
+++ Assignments/Notes/arrays.cpp
@@ -0,0 +1,25 @@
+/*
+Arrays: data storage device for multiple pieces of data of the same type
+- E.x daily pill container/organizer 
+[M][T][W][Th][F][Sa][Su]
+ 0  1  2  3   4  5   6
+
+Syntax:
+type name[size];
+
+int grades[5] = {79, 85, 94, 80, 100};
+
+Index: the location of data in an array. (0 indexed: 0, 1, 2, 3, 4)
+
+//Access value from array at index 1 (Second number)
+cout << "The second grade is " << grades[1] << endl; 
+
+//returns: "The second grade is 85"
+====================
+
+cout << "The grades for the last quarter were " << endl;
+
+
+
+
+*/
