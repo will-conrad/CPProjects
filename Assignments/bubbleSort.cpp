ef1c5fd7ecb90f9a1ca06fd57365fb60825ce651
Will Conrad (PC) <jwconrad23@gmail.com>
2021-05-01 13:36:20 -0400

    merge

--- Assignments/bubbleSort.cpp
+++ Assignments/bubbleSort.cpp
@@ -1,1 +1,38 @@
-ss
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+#define newl cout<<endl;
+
+
+int main() {
+	srand(time(NULL));
+	const int num = 100;
+	bool swapped;
+
+	int grades[num];
+	printf("Unsorted Array: ");
+	for(int i = 0; i < num; i++) {
+		grades[i] = rand() % 101; //Rand nums
+		printf("%i,", grades[i]); //Output unsorted
+	}
+	newl;
+	
+	do {
+		swapped = false;
+		for(int i = 0; i < num-1; i++) {
+			if (grades[i] > grades[i+1]) {
+				swap(grades[i], grades[i+1]);
+				swapped = true;
+			}
+		}
+	} while(swapped == true);
+	printf("Sorted Array: ");             
+	for(int i = 0; i < num; i++) {
+		printf("%i,", grades[i]); //Output unsorted
+	}
+
+	
+	return 0;
+}
+
