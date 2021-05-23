3f54c18248287045e9ec8f6ed4cc37609aecf453
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-05-23 15:17:28 -0400

    cout testing

--- cout2.cpp
+++ cout2.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main() {
 	int x = 32;
-	cout << "██";
-	cout << "\033["<<x<<";41m▞▞\033[0m\n";
+	string test[1];
+
+	
+	test[0] ="\033[41m▞▞\033[0m\n";
+	cout << test[0] << endl;
 	
 	return 0;
 }
