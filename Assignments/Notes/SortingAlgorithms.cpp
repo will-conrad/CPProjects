89c46f21711162505aa4b71e488b3e57e487de24
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-06-01 10:49:01 -0400

    formatting

--- Assignments/Notes/SortingAlgorithms.cpp
+++
@@ -1,9 +0,0 @@
-Algorithm: step by step process used to solve a problem or accomplish a task
-1. Bubble Sort
-- comparing 2 values and swapping them if they are out of order
-
-2. Selection Sort
-- comparing minimum values and swapping
-- backwards bubble sort (ish)
-
-3. Insertion Sort
--- Assignments/Notes/arrays.cpp
+++ Assignments/Notes/arrays.cpp
@@ -6,30 +6,23 @@ Arrays: data storage device for multiple pieces of data of the same type
 
 Syntax:
 type name[size];
-
+*/
 int grades[5] = {79, 85, 94, 80, 100};
 
-Index: the location of data in an array. (0 indexed: 0, 1, 2, 3, 4)
+//Index: the location of data in an array. (0 indexed: 0, 1, 2, 3, 4)
 
 //Access value from array at index 1 (Second number)
-cout << "The second grade is " << grades[1] << endl; 
+cout << "The second grade is " << grades[1] << endl;
 
 //returns: "The second grade is 85"
-====================
+//====================
 
 cout << "The grades from the last quarter were " << endl;
 for (int i = 0; i < 5; i++) {
 	cout << grades[i] << ", ";
 }
+
 //Returns: 
-The grades from the last quarter were 79, 85, 94, 80, 100, 
+//The grades from the last quarter were 79, 85, 94, 80, 100, 
 
-====================
-
-int finalGrade
-
-
-
-
-
-*/
+//====================
