440afd090187979ec8c9deee06723c230f770fee
Will Conrad (Mac) <jwconrad23@gmail.com>
2021-06-02 18:35:09 -0400

    New testing

--- cout2.cpp
+++ cout2.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
+#include<cmath> 
 using namespace std;
 
 int main() {
-	int x = 32;
-	string test[1];
-
-	
-	test[0] ="\033[41m▞▞\033[0m\n";
-	cout << test[0] << endl;
-	
+	long number;
+	cin >> number;
+	int size = trunc(log10(number)) + 1;
+	int array[size];
+	for (int i = size - 1; i >= 0; i--) {
+	    array[i] = number % 10;
+	    number /= 10;
+	}
+	for (int i = 0; i < size; i++) {
+		cout << array[i];
+	}
+	cout <<endl;
+	cout << size <<endl;	
 	return 0;
 }
